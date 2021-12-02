4034a917a809707777304cfbda6efdc894903255
hoa <hoa@gmail.com>
2021-12-02 18:11:44 +0700

    dasd

--- src/pkg1ServiceProvider.php
+++ src/pkg1ServiceProvider.php
@@ -6,11 +6,11 @@ use Illuminate\Support\ServiceProvider;
 class ContactFormServiceProvider extends ServiceProvider {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'hello');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'hello');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/hello'),
+            __DIR__.'/resources/views' => resource_path('views/vendor/hello'),
         ]);
 
 
