58b67cae38edeb692ce46489b6b43bc4bf67c7de
hoa <hoa@gmail.com>
2021-12-02 18:16:59 +0700

    dasd

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test1' , [\App\Http\Controllers\TestController::class , 'index']);
+Route::get('/test1' , [TestController::class , 'index']);
